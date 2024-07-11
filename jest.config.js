613ddf40ab07820e8d975436066c7d1f8ee8afe9
Devid030 <duongdevid098@gmail.com>
2024-07-11 14:14:13 +0700

    'ci'

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/__test__/*.ts?(x)',
+    '!**/node_modules/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  coverageReporters: ['text-summary', 'lcov'],
+};
